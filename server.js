62eda82110cefcc5e78db7c28f7f090b1b1093f3
Koby <kobysac@gmail.com>
2023-10-22 22:22:18 -0700

    completed file structure

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
