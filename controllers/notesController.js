4ab199fe3506073321fde4377923c704d6b2c33b
Koby <kobysac@gmail.com>
2023-10-23 22:53:55 -0700

    can post a new note, find uuid npm module

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,3 +1,5 @@
+const fs = require('fs/promises');
+const Note = require('../lib/Note');
 const notes = require('../db/db.json');
 
 
@@ -6,8 +8,32 @@ const getNotes = (req, res) => {
     res.json(notes);
 }
 
-const createNote = (req, res) => {
-    res.send("Not creating note. COME BACK LATER!")
+const createNote = async (req, res) => {
+
+    const {title, text} = req.body;
+
+    if(!title) {
+        return res.status(400).json({error: "Title is required"})
+    }
+
+    if(!text) {
+        return res.status(400).json({error: "Text is required"})
+    }
+
+    const newNote = new Note(title, text);
+
+    notes.push(newNote);
+
+    try {
+        const promise = fs.writeFile('./db/db.json',JSON.stringify(notes, null, 4));
+      
+        await promise;
+        res.send("Not creating note. COME BACK LATER!");
+      } catch (err) {
+        // When a request is aborted - err is an AbortError
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+      }
 }
 
 const editNote = (req, res) => {
